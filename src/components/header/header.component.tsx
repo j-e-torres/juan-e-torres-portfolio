609c8a655d9c7a4acd24df5c54c574d152d04029
Juan Torres <juan.torres@halopowered.com>
2023-11-26 14:28:50 -0500

    feat(header): migrate assets, and social media scss

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,21 +1,25 @@
-import { Nav } from '../nav/nav.component';
+import {Nav} from '../nav/nav.component';
 import './header.styles.scss';
 
 export function Header() {
   return (
-    <header className='header util-mb-4'>
+    <header className="header util-mb-4">
       <Nav />
 
-      <h1 className='heading-primary heading-primary--main'>Juan Torres</h1>
-      <h2 className='heading-primary heading-primary--sub'>Full Stack Developer</h2>
+      <h1 className="heading-primary heading-primary--main">Juan Torres</h1>
+      <h2 className="heading-primary heading-primary--sub util-mb-10">Full Stack Developer</h2>
 
-      <div>
-        <a>
-          <svg></svg>
+      <div className="social-media">
+        <a rel="noopener" target="_blank" href="https://github.com/j-e-torres" className="social-media__link">
+          <svg className="social-media__icon social-media__icon--default">
+            <use xlinkHref="./assets/img/sprite.svg#icon-github"></use>
+          </svg>
         </a>
 
-        <a>
-          <svg></svg>
+        <a rel="noopener" target="_blank" href="https://www.linkedin.com/in/juan-torres-w2w2" className="social-media__link">
+          <svg className="social-media__icon social-media__icon--default">
+            <use xlinkHref="./assets/img/sprite.svg#icon-linkedin"></use>
+          </svg>
         </a>
       </div>
     </header>
