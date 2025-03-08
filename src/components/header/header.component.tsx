837075f880f40ad27953818090a1d020bc47b661
Juan E Torres <juanet1453@gmail.com>
2025-03-07 23:13:31 -0500

    update header job title and add branding

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -10,8 +10,9 @@ export function Header() {
     <header className="header">
       <Nav />
 
-      <h1 className="heading-primary heading-primary--main">Juan Torres</h1>
-      <h2 className="heading-primary heading-primary--sub util-mb-10">Full Stack Developer</h2>
+      <h1 className="heading-primary heading-primary--main">Juan Enrique Torres</h1>
+      <h2 className="heading-primary heading-primary--sub util-mb-2">Software Engineer | Technical Mentor</h2>
+      <h3 className="heading-primary heading-primary--sub-2 util-mb-10">Empowering Businesses & People Through Technology</h3>
 
       <div className="social-media">
         <a rel="noopener" target="_blank" href="https://github.com/j-e-torres" className="social-media__link">
